e0aefb28b1ff38e3f196d30e10e8e92a31fe4f91
alyrraza <f223399@cfd.nu.edu.pk>
2023-12-03 02:06:54 -0800

    CHANED

--- dictionaryyy/Form1.Designer.cs
+++ dictionaryyy/Form1.Designer.cs
@@ -43,9 +43,10 @@
             richTextBox1.Font = new Font("Times New Roman", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             richTextBox1.ForeColor = Color.Teal;
             richTextBox1.Location = new Point(0, 0);
+            richTextBox1.Margin = new Padding(4, 5, 4, 5);
             richTextBox1.Name = "richTextBox1";
             richTextBox1.ReadOnly = true;
-            richTextBox1.Size = new Size(476, 53);
+            richTextBox1.Size = new Size(678, 86);
             richTextBox1.TabIndex = 0;
             richTextBox1.TabStop = false;
             richTextBox1.Text = "                     Welcome to Your Dictionary";
@@ -57,11 +58,12 @@
             richTextBox2.Cursor = Cursors.Hand;
             richTextBox2.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             richTextBox2.ForeColor = SystemColors.Info;
-            richTextBox2.Location = new Point(12, 24);
+            richTextBox2.Location = new Point(17, 40);
+            richTextBox2.Margin = new Padding(4, 5, 4, 5);
             richTextBox2.Name = "richTextBox2";
             richTextBox2.ReadOnly = true;
             richTextBox2.ScrollBars = RichTextBoxScrollBars.None;
-            richTextBox2.Size = new Size(452, 20);
+            richTextBox2.Size = new Size(646, 33);
             richTextBox2.TabIndex = 1;
             richTextBox2.TabStop = false;
             richTextBox2.Text = "             Explore words with ease in our app's handy dictionary!";
@@ -71,9 +73,10 @@
             // 
             button1.FlatStyle = FlatStyle.Flat;
             button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button1.Location = new Point(2, 50);
+            button1.Location = new Point(3, 83);
+            button1.Margin = new Padding(4, 5, 4, 5);
             button1.Name = "button1";
-            button1.Size = new Size(151, 64);
+            button1.Size = new Size(216, 107);
             button1.TabIndex = 2;
             button1.Text = "Home";
             button1.UseVisualStyleBackColor = true;
@@ -82,9 +85,10 @@
             // 
             button2.FlatStyle = FlatStyle.Flat;
             button2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button2.Location = new Point(149, 50);
+            button2.Location = new Point(213, 83);
+            button2.Margin = new Padding(4, 5, 4, 5);
             button2.Name = "button2";
-            button2.Size = new Size(175, 64);
+            button2.Size = new Size(250, 107);
             button2.TabIndex = 3;
             button2.Text = "Search";
             button2.UseVisualStyleBackColor = true;
@@ -95,9 +99,10 @@
             button3.FlatStyle = FlatStyle.Flat;
             button3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
             button3.ForeColor = SystemColors.ActiveCaptionText;
-            button3.Location = new Point(321, 50);
+            button3.Location = new Point(459, 83);
+            button3.Margin = new Padding(4, 5, 4, 5);
             button3.Name = "button3";
-            button3.Size = new Size(153, 64);
+            button3.Size = new Size(219, 107);
             button3.TabIndex = 4;
             button3.Text = "About";
             button3.UseVisualStyleBackColor = true;
@@ -105,23 +110,26 @@
             // 
             // textBoxLoad
             // 
-            textBoxLoad.Location = new Point(12, 135);
+            textBoxLoad.BackColor = SystemColors.InactiveCaptionText;
+            textBoxLoad.Location = new Point(262, 235);
+            textBoxLoad.Margin = new Padding(4, 5, 4, 5);
             textBoxLoad.Name = "textBoxLoad";
-            textBoxLoad.Size = new Size(100, 23);
+            textBoxLoad.Size = new Size(141, 31);
             textBoxLoad.TabIndex = 5;
             textBoxLoad.TextChanged += textBoxLoad_TextChanged;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(476, 288);
+            ClientSize = new Size(680, 480);
             Controls.Add(textBoxLoad);
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(richTextBox2);
             Controls.Add(richTextBox1);
+            Margin = new Padding(4, 5, 4, 5);
             MaximizeBox = false;
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
